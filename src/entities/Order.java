64027ec0edcdab025c5d596ac0457c7fb0fbe1de
Elves Aguiar <elvesmhs@hotmail.com>
2022-04-06 13:34:18 -0300

    Criando Order e OrderStatus

---
+++ src/entities/Order.java
@@ -0,0 +1,20 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import enums.OrderStatus;
+
+public class Order {
+	private Date moment;
+	private OrderStatus status;
+	
+	
+	public Order() {
+		
+	}
+	
+	
+	
+	
+}
